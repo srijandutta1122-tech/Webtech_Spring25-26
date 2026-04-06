b1a4043a0eb175e2c5ae598cc98e1f8d25585a3b
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-04-06 20:31:30 +0600

    add input

--- Lab_performance_Mid/JS/Script.js
+++ Lab_performance_Mid/JS/Script.js
@@ -2,6 +2,7 @@ console.log("connected");
 window.onload = function () 
 { 
   var unitPrice = 1000;
-
+  var quantityInput = document.getElementById("QuantityPerDay");
+  var totalInput = document.getElementById("TotalPrice");
 
  }
