2ae606375f8a53bdc6513477c4b657e1013f3481
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-04-24 21:49:03 +0600

    file in xamp

--- FINAL/Lab Practice/view/Registration.php
+++ FINAL/Lab Practice/view/Registration.php
@@ -3,4 +3,19 @@
 <head>
     <title>Registration</title>
 </head>
+<body>
+    <h1>Registration</h1>
+    <form action="process_registration.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <input type="submit" value="Register">
+    </form>
+</body>
 
