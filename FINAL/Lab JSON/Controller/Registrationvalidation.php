4f4b21c54500b61be2fee7d78e2902973a86059c
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-04-25 11:51:27 +0600

    post data

--- FINAL/Lab JSON/Controller/Registrationvalidation.php
+++ FINAL/Lab JSON/Controller/Registrationvalidation.php
@@ -5,4 +5,9 @@ $email= "";
 $website = "";
 $gender = "";
 $datafile ="../data.json";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $website = $_POST['website'];
+    $gender = $_POST["gender"];
 ?>
